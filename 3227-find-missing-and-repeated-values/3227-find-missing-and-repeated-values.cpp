2060b303d6ccf1f238dafbed7998381622950082
Prince Donda <120329882+pdonda06@users.noreply.github.com>
2025-01-22 23:43:49 +0530

    Time: 4 ms (77.5%), Space: 27.8 MB (72.97%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         vector<int>res;
 
-        for(const auto &ro: grid){
+        for(const auto ro: grid){
             for(auto i: ro){
                 res.push_back(i);
             }
@@ -11,6 +11,7 @@ public:
 
         sort(res.begin(),res.end());
         vector<int>ans(2,0);
+        
         for(int i=1;i<res.size();i++){
             if(res[i] == res[i-1]){
                 ans[0] = res[i];
