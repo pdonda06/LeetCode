75c6025b3573ab3139d3941e6eb51e300bada83e
Prince Donda <120329882+pdonda06@users.noreply.github.com>
2025-01-22 23:38:26 +0530

    Time: 9 ms (70.23%), Space: 27 MB (74.97%) - LeetHub

---
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        vector<int>res;
+
+        for(const auto &ro: grid){
+            for(auto i: ro){
+                res.push_back(i);
+            }
+        }
+
+        sort(res.begin(),res.end());
+        vector<int>ans(2,0);
+        for(int i=1;i<res.size();i++){
+            if(res[i] == res[i-1]){
+                ans[0] = res[i];
+            }
+        }
+
+        int n = res.size();
+        int sum = accumulate(res.begin(), res.end(), 0);
+        int rsum = (n*(n+1))/2;
+        
+        int dif = rsum - sum;
+
+        ans[1] = dif + ans[0];
+
+
+        return ans;
+    }
+};
