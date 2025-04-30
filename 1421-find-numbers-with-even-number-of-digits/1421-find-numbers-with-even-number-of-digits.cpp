4d67dc7a48081f2e0a2bb86cb4e5463564a969a9
Prince Donda <120329882+pdonda06@users.noreply.github.com>
2025-04-30 17:24:16 +0530

    Time: 0 ms (100%), Space: 14.4 MB (11.46%) - LeetHub

---
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int n = nums.size();
+        int cnt = 0;
+
+        for(int i = 0; i<n; i++){
+            string s = to_string(nums[i]);
+
+            if(s.length() % 2 == 0){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
