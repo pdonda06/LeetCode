0c143329d7555ee6b267c2c3657844589f6ede30
Prince Donda <120329882+pdonda06@users.noreply.github.com>
2025-05-21 14:23:17 +0530

    Time: 24 ms (35.6%), Space: 48.5 MB (61.77%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -5,8 +5,8 @@ public:
         vector<int> answer;
 
         int ans = 0;
-        for(int i=0;i<nums.size()-1;i++){
-            ans = nums[i]^nums[i+1];
+        for(int i =0; i<nums.size() - 1; i++){
+            ans = nums[i] ^ nums[i+1];
             if(ans == 0){
                 answer.push_back(nums[i]);
             }
