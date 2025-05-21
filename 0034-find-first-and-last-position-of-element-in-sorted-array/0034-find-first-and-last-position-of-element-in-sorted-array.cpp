48341f7018e470924daca47ebdf9b51ba554a9d1
Prince Donda <120329882+pdonda06@users.noreply.github.com>
2025-05-21 15:18:41 +0530

    Time: 0 ms (100%), Space: 17.6 MB (17.16%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -11,30 +11,37 @@ public:
         while(start <= end){
             if(target == nums[mid]){
                 index1 = mid;
-                end = mid-1;
+                end = mid -1;
             }
-            else if(target < nums[mid]){
-                end = mid-1;
+            else if(target > nums[mid]){
+                start = mid + 1;
             }
-            else start = mid+1;
+            else{
+                end = mid -1;
+            }
             mid = start + (end-start)/2;
         }
 
-        start = 0; end = n-1; mid = start + (end-start)/2;
+        start = 0, end = n-1, mid;
+        mid = start + (end-start)/2;
 
-        //finding last occurance 
+        //finding last occurance
         while(start <= end){
             if(target == nums[mid]){
                 index2 = mid;
-                start = mid+1;
+                start = mid +1;
             }
-            else if(target < nums[mid]){
-                end = mid-1;
+            else if(target > nums[mid]){
+                start = mid + 1;
             }
-            else start = mid+1;
+            else{
+                end = mid -1;
+            }
             mid = start + (end-start)/2;
         }
 
+
+        
         ans.push_back(index1);
         ans.push_back(index2);
         return ans;
