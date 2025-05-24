020dfaef325d5db4ea840d71b796d3965e5eb887
Prince Donda <120329882+pdonda06@users.noreply.github.com>
2025-05-24 15:41:51 +0530

    Time: 1 ms (56.33%), Space: 12.8 MB (58.69%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        //sub optimal
         int n = nums1.size();
-        vector<int> ans(n, -1);
+        vector<int> ans(n, -1), nge(nums2.size(), -1);
         unordered_map<int , int> mp;
 
         for (int i = 0; i < nums2.size(); i++) {
             mp[nums2[i]] = i;
         }
+        stack<int> st;
 
-        for(int i =0;i<n;i++){
-            int j = mp[nums1[i]]+1;
-            for(;j<nums2.size();j++){
-                if(nums2[j] > nums1[i]){
-                    ans[i] = nums2[j];
-                    break;
-                }
-            }
+        for(int i = nums2.size() -1; i>=0; i--){
+            while(st.size() > 0 && nums2[i] >= st.top()) st.pop();
+
+            if(st.size() ) nge[i] = st.top();
+
+            st.push(nums2[i]);
         }
 
+        for(int i =0; i<n; i++){
+            ans[i] = nge[mp[nums1[i]]];
+        }
         return ans;
     }
 };
