f364a5b6da68f3879c6fabe936cde394213f0b78
Prince Donda <120329882+pdonda06@users.noreply.github.com>
2025-05-25 13:25:40 +0530

    Time: 0 ms (100%), Space: 9.5 MB (55.05%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,48 @@
+class MyStack {
+private:
+    queue<int> q1;
+    queue<int> q2;
+
+public:
+    MyStack() {
+    }
+    
+    void push(int x) {
+        q1.push(x);
+    }
+    
+    int pop() {
+        while (q1.size() > 1) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+
+        int topElement = q1.front();
+        q1.pop();
+
+        swap(q1, q2);
+
+        return topElement;
+    }
+    
+    int top() {
+        int topElement;
+
+        while (q1.size() > 1) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+
+        topElement = q1.front();
+        q2.push(topElement);
+        q1.pop();
+
+        std::swap(q1, q2);
+
+        return topElement;
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
