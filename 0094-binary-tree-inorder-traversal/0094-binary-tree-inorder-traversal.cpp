990c79a4b0dbe3ebd496544ce8f50d34d9fe674f
Prince Donda <120329882+pdonda06@users.noreply.github.com>
2025-05-26 13:16:08 +0530

    Time: 2 ms (5.02%), Space: 12.5 MB (6.68%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -13,12 +13,14 @@ class Solution {
 public:
     vector<int> ans;
     vector<int> inorderTraversal(TreeNode* root) {
-       if (!root)
+        if(!root){
             return {};
+        }
+
         inorderTraversal(root->left);
         ans.push_back(root->val);
         inorderTraversal(root->right);
-        return ans;
 
+        return ans;
     }
 };
