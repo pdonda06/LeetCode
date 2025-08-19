f3238998a53bde0922158172566996d7312fdc30
Prince Donda <120329882+pdonda06@users.noreply.github.com>
2025-08-19 12:15:02 +0530

    Time: 0 ms (100%), Space: 111.4 MB (40.61%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
-        long long cnt = 0, streak = 0;
-        for (int num : nums) {
-            if (num == 0) {
-                streak++;
-                cnt += streak;
-            } else {
-                streak = 0;
+        long long cnt = 0, ans = 0;
+
+        for(int num : nums){
+            if(num == 0){
+                ans++;
+                cnt += ans;
+            }
+            else{
+                ans = 0;
             }
         }
+
         return cnt;
     }
 };
